8d0bd3934a1dd8b889e15675a2f757a6b4640ba3
Daniel Nkemelu <daniel.nkemelu@gmail.com>
2020-12-18 14:02:19 -0500

    Added code to pull twitter data

---
+++ disinfo.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from searchtweets import load_credentials
+
+premium_search_args = load_credentials("apicred.yaml",
+                                       yaml_key="search_tweets_api",
+                                       env_overwrite=False)
+
+print(premium_search_args)
+
+
+# In[18]:
+
+
+from searchtweets import gen_rule_payload 
+
+# query = "(covid OR covid19)"
+
+query = "(covid OR covid19) lang:en place_country:NG"
+
+rule = gen_rule_payload(query, results_per_call=100, from_date="2020-01-01", to_date="2020-10-31")
+
+print(rule)
+
+
+# In[19]:
+
+
+from searchtweets import ResultStream
+
+rs = ResultStream(rule_payload=rule,
+                  max_results=100,
+                  **premium_search_args)
+print(rs)
+
+
+# In[20]:
+
+
+import json
+
+with open('test.jsonl', 'a', encoding='utf-8') as f:
+    for tweet in rs.stream():
+        json.dump(tweet, f)
+        f.write('\n')
+        
+print('done')
+
+
+# In[ ]:
+
+
+
+
